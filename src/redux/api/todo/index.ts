89a6dd03e83dd238ce91a9383e78e87b96630836
shaiyldaevich <salamalikbaryktabasov@gmail.com>
2024-09-09 14:35:29 +0600

    rtq query

---
+++ src/redux/api/todo/index.ts
@@ -0,0 +1,22 @@
+import { api as index } from '..';
+
+const api = index.injectEndpoints({
+	endpoints: (build) => ({
+		getTodo: build.query({
+			query: () => ({
+				url: '/marchik',
+				method: 'GET'
+			}),
+			providesTags: ['todo']
+		}),
+        postTodo:build.mutation({
+            query: (newData) => ({
+                url: '/marchik',
+                method: 'POST',
+                body: {newData: newData}
+            }),
+            invalidatesTags: ['todo']
+        })
+	})
+});
+export const { useGetTodoQuery} = api;
